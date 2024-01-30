5ee05ffa3bedfc3882231876b0e941dd5e8d89f9
JulianKominovic <juliankominovic@gmail.com>
2024-01-30 19:18:45 -0300

    feat: macos configs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-pub mod events;
-use events::greet;
-use specta::collect_types;
-use tauri_specta::ts;
-fn main() {
-    #[cfg(debug_assertions)]
-    ts::export(collect_types![greet], "../src/bindings.ts").unwrap();
+use tauri::Manager;
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
 
+fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(move |app| {
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(
+                &window,
+                NSVisualEffectMaterial::WindowBackground,
+                None,
+                None,
+            )
+            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+            #[cfg(target_os = "windows")]
+            apply_blur(&window, Some((18, 18, 18, 125)))
+                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
